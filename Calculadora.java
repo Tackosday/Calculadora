9f903c977ba426ee6e09e529983f817c3f6140db
Tackosday <juandaesca0@gmail.com>
2025-09-25 15:22:27 -0500

    Feat:Creacion de funciones de modulo, resta y factorial, con su debido fucionamiento en main

--- Calculadora.java
+++ Calculadora.java
@@ -8,5 +8,10 @@ public class Calculadora {
         System.out.println("Resultado de la suma: " + calc.sumar());
         System.out.println("Resultado de la multiplicación: " + calc.multiplicar());
         System.out.println("Resultado de la división: " + calc.dividir());
+        System.out.println("Resultado del módulo: " + calc.modulo());
+        System.out.println("Resultado de la resta: " + calc.restar());
+        int[] factoriales = calc.factorial();
+        System.out.println("Factorial de 1: " + factoriales[0]);
+        System.out.println("Factorial de 2: " + factoriales[1]);
     }
 }
--- FuncionesCalculadora.java
+++ FuncionesCalculadora.java
@@ -20,4 +20,28 @@ public class FuncionesCalculadora {
         return this.valorA * this.valorB;
     }
 
+    public int modulo() {
+        return this.valorA % this.valorB;
+    }
+
+    public int[] factorial() {
+        int[] factoriales = new int[2];
+        factoriales[0] = 1;
+        factoriales[1] = 1;
+
+        for (int i = 1; i <= valorA; i++) {
+            factoriales[0] *= i;
+        }
+
+        for (int i = 1; i <= valorB; i++) {
+            factoriales[1] *= i;
+        }
+
+        return factoriales;
+    }
+
+    public int restar() {
+        return this.valorA - this.valorB;
+    }
+
 }
