7bad39b53113e26d42ca11c6db60dbf48dbb9e5a
PersWolf <danielfuentesmusic04@gmail.com>
2025-09-24 22:07:05 -0500

    creacion de funcion para sumar. :D

--- FuncionesCalculadora.java
+++ FuncionesCalculadora.java
@@ -1,7 +1,11 @@
 public class FuncionesCalculadora {
+	
+	// Funcion para sumar by: PersWolf
     
-private int valorA;
-private int valorB;
-
-
+    private int valorA;
+    private int valorB;
+    
+    public int sumar() {
+        return this.valorA + this.valorB;
+    }
 }
