63c2833b2d883461361c1e008a7e5b55e48d811e
Tackosday <juandaesca0@gmail.com>
2025-09-24 22:12:11 -0500

    creacion de funcion de divicion

--- Calculadora.java
+++ Calculadora.java
@@ -2,5 +2,7 @@ public class Calculadora{
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        FuncionesCalculadora calc = new FuncionesCalculadora(10, 2);
+        System.out.println("Resultado de la división: " + calc.dividir());
     }
 }
