e80291c0c76343a683c10e21d12045fcab09b50d
PersWolf <danielfuentesmusic04@gmail.com>
2025-09-25 19:10:56 -0500

    Agregue la funcion Logaritmica para la calculadora :D

--- FuncionesCalculadora.java
+++ FuncionesCalculadora.java
@@ -8,4 +8,20 @@ public class FuncionesCalculadora {
     public int sumar() {
         return this.valorA + this.valorB;
     }
+    public int Logaritmo(){
+        if (valorA<=0) {
+            throw new IllegalAccessException(s:"Error, El valor debe ser mayor que 0.")
+        }
+        if (valorB<=1){
+            throw new IllegalAccessException(s:"Error, La base debe ser mayor que 1.")
+        }
+        int resultado = 0;
+        int potencia = 1;
+
+        while (potencia *valorB <= valorA) {
+            potencia *= valorB;
+            resultado ++;
+        }
+        return resultado;
+    }
 }
